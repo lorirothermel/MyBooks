bce5c7f99444c9049477b51a43030a76bfed6dc4
Lori Rothermel <lori@rothermel1.com>
2024-11-05 18:26:46 -0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Lori Rothermel on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
