7388f69ad00763e38407352d47fab344829d921e
Lori Rothermel <lori@rothermel1.com>
2024-11-08 19:00:31 -0800

    Section 3 - Dynamic Sorts and Filters

---
+++ MyBooks/Views/BookList.swift
@@ -0,0 +1,18 @@
+//
+//  BookList.swift
+//  MyBooks
+//
+//  Created by Lori Rothermel on 11/8/24.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookList()
+}
