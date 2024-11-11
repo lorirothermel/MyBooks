c88fa545bf801d35ac63aed83710e50eda5f3021
Lori Rothermel <lori@rothermel1.com>
2024-11-11 11:57:01 -0800

    Series 5 - SwiftData One to Many Relationships

---
+++ MyBooks/Models/Quote.swift
@@ -0,0 +1,8 @@
+//
+//  Quote.swift
+//  MyBooks
+//
+//  Created by Lori Rothermel on 11/9/24.
+//
+
+import Foundation
---
+++ MyBooks/Views/QuotesListView.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesListView.swift
+//  MyBooks
+//
+//  Created by Lori Rothermel on 11/9/24.
+//
+
+import SwiftUI
+
+struct QuotesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuotesListView()
+}
